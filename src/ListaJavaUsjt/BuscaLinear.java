5611114665bb8609e29a4d55e83d1854389c3dc7
Geovanna Guedes <geovanna.guedes2004@gmail.com>
2024-03-18 21:52:48 -0300

    adição de matrizes e busca linear

--- src/ListaJavaUsjt/BuscaLinear.java
+++ src/ListaJavaUsjt/BuscaLinear.java
@@ -1,5 +1,8 @@
 package ListaJavaUsjt;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BuscaLinear {
 
     /* 32o) Elaborar um programa, em linguagem Java, capaz de carregar, em um vetor do tipo
@@ -11,9 +14,48 @@ public class BuscaLinear {
     número de busca não seja localizado). A busca linear deverá ser um método que se utilize
     de passagem de parâmetros e o vetor não deverá ser variável global do programa.
     */
-    public static void exercicio32() {
 
+    public static void exercicio32() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Criando vetor para armazenar os números
+        int[] vetor = new int[15];
+
+        // Pedindo ao usuário para digitar os números
+        System.out.println("Digite 15 números:");
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        // Pedindo ao usuário para digitar o número de busca
+        System.out.print("Digite o número que deseja buscar: ");
+        int numeroBusca = scanner.nextInt();
+
+        // Chamando o método de busca linear
+        int indice = buscaLinear(vetor, numeroBusca);
+
+        // Verificando o resultado da busca
+        if (indice != -1) {
+            System.out.println("O número " + numeroBusca + " foi encontrado no índice " + indice + " do vetor.");
+        } else {
+            System.out.println("O número " + numeroBusca + " não foi encontrado no vetor.");
+        }
     }
+
+    // Método para busca linear
+    public static int buscaLinear(int[] vetor, int numeroBusca) {
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == numeroBusca) {
+                return i; // Retorna o índice onde o número foi encontrado
+            }
+        }
+        return -1; // Retorna -1 se o número não for encontrado no vetor
+    }
+
+
+
 
     /* 33o) Elaborar um programa, em linguagem Java, capaz de carregar uma String, com 15
     caracteres, digitada, em um vetor do tipo char, com 15 posições, formando uma espécie
@@ -26,8 +68,50 @@ public class BuscaLinear {
     uma variável global do programa.
     */
     public static void exercicio33() {
-
-    }
+            Scanner scanner = new Scanner(System.in);
+    
+            // Carregar a String em um vetor de char
+            char[] vetor = new char[15];
+            System.out.println("Digite uma string de 15 caracteres:");
+            String inputString = scanner.nextLine();
+    
+            // Verificar se a string digitada possui 15 caracteres
+            if (inputString.length() != 15) {
+                System.out.println("A string digitada não possui 15 caracteres. Por favor, tente novamente.");
+                return;
+            }
+    
+            // Preencher o vetor com os caracteres da string
+            for (int i = 0; i < 15; i++) {
+                vetor[i] = inputString.charAt(i);
+            }
+    
+            // Solicitar o caractere de busca
+            System.out.println("Digite o caractere que deseja buscar:");
+            char caractereBusca = scanner.next().charAt(0);
+    
+            // Realizar a busca linear
+            int indice = buscaLinear(vetor, caractereBusca);
+    
+            // Exibir o resultado da busca
+            if (indice != -1) {
+                System.out.println("O caractere " + caractereBusca + " foi encontrado no índice " + indice + ".");
+            } else {
+                System.out.println("O caractere " + caractereBusca + " não foi encontrado no vetor.");
+            }
+        }
+    
+        // Método para realizar busca linear em um vetor de char
+        public static int buscaLinear(char[] vetor, char caractereBusca) {
+            for (int i = 0; i < vetor.length; i++) {
+                if (vetor[i] == caractereBusca) {
+                    return i;
+                }
+            }
+            // Retorna -1 se o caractere não for encontrado
+            return -1;
+        }
+    
 
     /* 34o) Elaborar um programa, em linguagem Java, capaz de carregar uma String, digitada
     em um vetor do tipo char, com no máximo 15 posições, formando uma espécie de base
@@ -40,7 +124,44 @@ public class BuscaLinear {
     parâmetros e o vetor não deverá ser uma variável global do programa.
     */
     public static void exercicio34() {
+        Scanner scanner = new Scanner(System.in);
 
+        // Solicitar ao usuário que digite a string
+        System.out.print("Digite uma string com no máximo 15 caracteres: ");
+        String input = scanner.nextLine();
+
+        // Verificar se a string tem mais que 15 caracteres
+        if (input.length() > 15) {
+            System.out.println("A string excede o limite de 15 caracteres.");
+            return;
+        }
+
+        // Transformar a string em um vetor de char
+        char[] vetor = input.toCharArray();
+
+        // Solicitar ao usuário que digite o caractere de busca
+        System.out.print("Digite o caractere de busca: ");
+        char busca = scanner.next().charAt(0);
+
+        // Realizar a busca linear
+        int resultado = buscaLinear(vetor, busca);
+
+        // Apresentar o resultado
+        if (resultado == -1) {
+            System.out.println("O caractere de busca não foi encontrado.");
+        } else {
+            System.out.println("O caractere de busca foi encontrado no índice: " + resultado);
+        }
+    }
+
+    // Método para busca linear
+    public static int buscaLinear2(char[] vetor, char busca) {
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == busca) {
+                return i; // Retorna o índice onde o caractere foi encontrado
+            }
+        }
+        return -1; // Retorna -1 se o caractere não for encontrado
     }
 
     /* 35o) Elaborar um programa, em linguagem Java, capaz de controlar os dados de uma
@@ -56,7 +177,45 @@ public class BuscaLinear {
     segundos e armazenado em variável do tipo int.
     */
     public static void exercicio35() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite a quantidade de corredores inscritos na prova:");
+        int quantidadeCorredores = scanner.nextInt();
+
+        // Declaração da matriz para armazenar as informações dos corredores
+        int[][] dadosCorredores = new int[quantidadeCorredores][3]; // [Nº de inscrição, posição de chegada, tempo de prova]
+
+        // Preenchimento dos dados dos corredores
+        for (int i = 0; i < quantidadeCorredores; i++) {
+            System.out.println("Digite o número de inscrição do corredor " + (i + 1) + ":");
+            dadosCorredores[i][0] = scanner.nextInt();
+
+            System.out.println("Digite a posição de chegada do corredor " + (i + 1) + ":");
+            dadosCorredores[i][1] = scanner.nextInt();
+
+            System.out.println("Digite o tempo de prova (em segundos) do corredor " + (i + 1) + ":");
+            dadosCorredores[i][2] = scanner.nextInt();
+        }
+
+        // Busca de informações de um corredor pelo número de inscrição
+        System.out.println("Digite o número de inscrição do corredor para buscar suas informações:");
+        int numeroInscricaoBusca = scanner.nextInt();
+        boolean encontrado = false;
+        for (int i = 0; i < quantidadeCorredores; i++) {
+            if (dadosCorredores[i][0] == numeroInscricaoBusca) {
+                encontrado = true;
+                System.out.println("Informações do corredor:");
+                System.out.println("Número de inscrição: " + dadosCorredores[i][0]);
+                System.out.println("Posição de chegada: " + dadosCorredores[i][1]);
+                System.out.println("Tempo de prova (em segundos): " + dadosCorredores[i][2]);
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Corredor não encontrado.");
+        }
+
+        scanner.close();
     }
 
     /* 36o) Elaborar um programa, em linguagem Java, que receba, via digitação, a quantidade
@@ -72,7 +231,83 @@ public class BuscaLinear {
     D) Sair do programa.
     */
     public static void exercicio36() {
+    
+    public static int[] raAlunos;
+    public static double[] notasAlunos;
+    public static int quantidadeAlunos;
+    public static Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Bem-vindo ao sistema da universidade!");
+
+        System.out.print("Digite a quantidade de alunos matriculados: ");
+        quantidadeAlunos = scanner.nextInt();
+        raAlunos = new int[quantidadeAlunos];
+        notasAlunos = new double[quantidadeAlunos];
+
+        int opcao;
+        do {
+            exibirMenu();
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    cadastrarRA();
+                    break;
+                case 2:
+                    cadastrarNotas();
+                    break;
+                case 3:
+                    buscarNotaPorRA();
+                    break;
+                case 4:
+                    System.out.println("Encerrando o programa. Até mais!");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha novamente.");
+            }
+        } while (opcao != 4);
+    }
+
+    private static void exibirMenu() {
+        System.out.println("\nEscolha uma opção:");
+        System.out.println("1) Cadastrar RA dos alunos");
+        System.out.println("2) Cadastrar notas dos alunos");
+        System.out.println("3) Buscar nota por RA");
+        System.out.println("4) Sair do programa");
+        System.out.print("Opção: ");
+    }
+
+    private static void cadastrarRA() {
+        System.out.println("\nDigite o RA de cada aluno:");
+        for (int i = 0; i < quantidadeAlunos; i++) {
+            System.out.print("RA do aluno " + (i + 1) + ": ");
+            raAlunos[i] = scanner.nextInt();
+        }
+        System.out.println("RA cadastrado com sucesso.");
+    }
+
+    private static void cadastrarNotas() {
+        System.out.println("\nDigite a nota de cada aluno:");
+        for (int i = 0; i < quantidadeAlunos; i++) {
+            System.out.print("Nota do aluno " + (i + 1) + ": ");
+            notasAlunos[i] = scanner.nextDouble();
+        }
+        System.out.println("Notas cadastradas com sucesso.");
+    }
+
+    private static void buscarNotaPorRA() {
+        System.out.print("\nDigite o RA do aluno que deseja buscar: ");
+        int raBusca = scanner.nextInt();
+        boolean encontrado = false;
+        for (int i = 0; i < quantidadeAlunos; i++) {
+            if (raAlunos[i] == raBusca) {
+                System.out.println("Nota do aluno com RA " + raBusca + ": " + notasAlunos[i]);
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("RA não encontrado.");
+        }
     }
 
     /* 37o) Em um serviço de check-in de uma companhia aérea, após a apresentação de seu
@@ -86,7 +321,125 @@ public class BuscaLinear {
     para cancelar todas as reservas de assento de uma só vez. Elaborar este programa.
     */
     public static void exercicio37() {
+ 
+class Assento {
+    private String rg;
+    private boolean ocupado;
 
+    public Assento() {
+        this.ocupado = false;
+    }
+
+    public String getRG() {
+        return rg;
+    }
+
+    public void reservar(String rg) {
+        this.ocupado = true;
+        this.rg = rg;
+    }
+
+    public void cancelarReserva() {
+        this.ocupado = false;
+        this.rg = null;
+    }
+
+    public boolean estaOcupado() {
+        return ocupado;
+    }
+}
+
+    private static ArrayList<Assento> assentos = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao serviço de check-in da companhia aérea!");
+
+        // Inicializando os assentos
+        for (int i = 0; i < 100; i++) {
+            assentos.add(new Assento());
+        }
+
+        while (true) {
+            System.out.println("\nOpções:");
+            System.out.println("1. Verificar próximo assento disponível");
+            System.out.println("2. Reservar assento");
+            System.out.println("3. Cancelar reserva de um assento");
+            System.out.println("4. Cancelar todas as reservas");
+            System.out.println("5. Sair");
+
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    verificarProximoAssentoDisponivel();
+                    break;
+                case 2:
+                    reservarAssento(scanner);
+                    break;
+                case 3:
+                    cancelarReservaAssento(scanner);
+                    break;
+                case 4:
+                    cancelarTodasReservas();
+                    break;
+                case 5:
+                    System.out.println("Obrigado por utilizar nosso serviço de check-in!");
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+
+    private static void verificarProximoAssentoDisponivel() {
+        for (int i = 0; i < assentos.size(); i++) {
+            if (!assentos.get(i).estaOcupado()) {
+                System.out.println("Próximo assento disponível: " + (i + 1));
+                return;
+            }
+        }
+        System.out.println("Todos os assentos estão ocupados.");
+    }
+
+    private static void reservarAssento(Scanner scanner) {
+        System.out.print("Digite o número do RG do passageiro: ");
+        String rg = scanner.next();
+
+        for (int i = 0; i < assentos.size(); i++) {
+            if (!assentos.get(i).estaOcupado()) {
+                assentos.get(i).reservar(rg);
+                System.out.println("Assento " + (i + 1) + " reservado para o passageiro com RG " + rg);
+                return;
+            }
+        }
+        System.out.println("Não há assentos disponíveis para reserva.");
+    }
+
+    private static void cancelarReservaAssento(Scanner scanner) {
+        System.out.print("Digite o número do assento a cancelar a reserva: ");
+        int numAssento = scanner.nextInt();
+
+        if (numAssento < 1 || numAssento > assentos.size()) {
+            System.out.println("Número de assento inválido.");
+            return;
+        }
+
+        Assento assento = assentos.get(numAssento - 1);
+        if (assento.estaOcupado()) {
+            assento.cancelarReserva();
+            System.out.println("Reserva do assento " + numAssento + " cancelada com sucesso.");
+        } else {
+            System.out.println("O assento selecionado não está ocupado.");
+        }
+    }
+
+    private static void cancelarTodasReservas() {
+        for (Assento assento : assentos) {
+            assento.cancelarReserva();
+        }
+        System.out.println("Todas as reservas foram canceladas.");
     }
 
     /* 38o) Elaborar um programa, em linguagem Java, que utilize apenas métodos iterativos,
@@ -100,9 +453,32 @@ public class BuscaLinear {
     Número Digitado: 4 → Resposta: O número 4 está localizado no índice 7 do vetor.
     */
     public static void exercicio38() {
+        Scanner scanner = new Scanner(System.in);
 
+        double[] vetor = new double[10];
+        System.out.println("Digite os 10 elementos do vetor:");
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = scanner.nextDouble();
+        }
+
+        System.out.println("Digite o elemento a ser buscado:");
+        double elementoBuscado = scanner.nextDouble();
+
+        int indice = buscarElemento(vetor, elementoBuscado);
+        if (indice != -1) {
+            System.out.println("O número " + elementoBuscado + " está localizado no índice " + indice + " do vetor.");
+        } else {
+            System.out.println("O número " + elementoBuscado + " não foi encontrado no vetor.");
+        }
     }
 
-    //Funcoes Auxiliares abaixo
-    
+    public static int buscarElemento(double[] vetor, double elemento) {
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == elemento) {
+                return i;
+            }
+        }
+        return -1; // Retorna -1 se o elemento não for encontrado no vetor
+    }
 }
+
